02a185124b65c0f468b33cbf3891730254d5deb0
Thompson Edolo <verygreenboi@live.com>
2019-09-07 16:36:33 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kobo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'common-components';
+}
